e8392659f032fd6ac78762a5013893c75f4e2f43
origin <mambo771@mail.ru>
2024-04-29 23:11:36 +0300

    Initial commit

---
+++ src/test/java/examweb/examweb/ExamWebApplicationTests.java
@@ -0,0 +1,13 @@
+package examweb.examweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
